ee0f97ea523d66e28bc07239a708c9181daaa4da
Florian <Florian@PC-Florian>
2016-04-23 23:28:53 +0200

    Initialer Commit

---
+++ src/iib2_gruppe1/Iib2_gruppe1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iib2_gruppe1;
+
+/**
+ *
+ * @author Florian Saumweber: 2354534
+ * @author Benjamin Krauß: xxxxxxx
+ */
+public class Iib2_gruppe1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
